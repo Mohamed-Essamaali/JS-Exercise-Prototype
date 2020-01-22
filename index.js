d52a3a1e38ba6550ea4dd4d7cdcece1dbf6b607e
Mohamed-Essamaali <mohamed.essamaali@gmail.com>
2020-01-21 21:30:14 -0500

    I completed 21 items and I am still missing one and the stretch

--- index.js
+++ index.js
@@ -39,9 +39,34 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+//constructor
+const Person = function (name,age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+ };
 
+//methods
+Person.prototype.eat = function(someFood){
+  if(this.stomach.length<10){
+   (this.stomach.push(someFood));
+  }
 }
+Person.prototype.poop = function(){
+        this.stomach =[];
+      }
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
+//create instances
+
+
+
+
+
+
+
 
 /*
   TASK 2
@@ -56,9 +81,20 @@ function Person() {
     - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
+//constructor
+const Car = function (model,milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank = 0;
+this.odometer = 0;
+}
+//methods
+Car.prototype.fill = function(gallons){
+  return this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(distance){
 
-function Car() {
-
+return this.odometer+distance;
 }
 
 /*
@@ -68,9 +104,16 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name,age,favoriteToy) {
+Person.call(this,name,age);
 
+this.favoriteToy= favoriteToy;
 }
+// Baby = new Person();
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
+
 
 /* 
   TASK 4
